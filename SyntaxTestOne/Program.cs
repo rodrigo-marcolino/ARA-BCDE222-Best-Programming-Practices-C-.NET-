6e2fe04d51a48988e13a0fa3acff5b0c3f2f7f66
unknown <rodrigomarcolino@gmail.com>
2020-09-09 16:01:35 +1200

    Add Task SyntaxTestOne

---
+++ SyntaxTestOne/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SyntaxTestOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
