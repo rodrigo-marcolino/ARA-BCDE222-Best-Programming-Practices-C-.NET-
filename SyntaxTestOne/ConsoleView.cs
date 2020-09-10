1bc05480fc238af45a8b391715794cb1e560f096
unknown <rodrigomarcolino@gmail.com>
2020-09-10 18:15:22 +1200

    add ConsoleView

---
+++ SyntaxTestOne/ConsoleView.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SyntaxTestOne
+{
+    class ConsoleView : IView
+    {
+        public void Display<T>(string message, T value)
+        {
+            Console.WriteLine(message, value);
+        }
+
+        public void Display<T>(T mesage)
+        {
+            Console.WriteLine(mesage);
+        }
+
+        public string GetInput(string prompt)
+        {
+            Console.WriteLine(prompt);
+            return Console.ReadLine();
+        }
+
+        public void Start()
+        {
+            Console.Clear();
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Press any key to Exit");
+            Console.ReadKey();
+        }
+    }
+}
--- SyntaxTestOne/IView.cs
+++ SyntaxTestOne/IView.cs
@@ -6,5 +6,10 @@ namespace SyntaxTestOne
 {
     interface IView
     {
+        string GetInput(string prompt);
+        void Display<T>(string message, T value);
+        void Display<T>(T mesage);
+        void Start();
+        void Stop();
     }
 }
